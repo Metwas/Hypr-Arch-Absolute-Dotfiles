3d377b709672d40b81274d45033754eaab5861db
Matt <metwas.seup@Gmail.com>
2026-01-03 22:47:44 +0000

    Update core.sh

--- scripts/core.sh
+++ scripts/core.sh
@@ -6,6 +6,11 @@ sudo pacman-key --populate archlinux
 sudo pacman -Sy --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
 sudo pacman -Sy --noconfirm iwd
+sudo pacman -S --noconfirm wine winetricks sdl2-compat gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg samba gnutls lib32-alsa-oss
+sudo pacman -S --noconfirm pipewire pipewire-pulse pipewire-alsa lib32-pipewire lib32-pipewire-pulse lib32-libpulse lib32-alsa-lib lib32-alsa-plugins
+sudo pacman -S --noconfirm vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa
+sudo pacman -S --noconfirm --needed gcc-libs lib32-gcc-libs glibc lib32-glibc
+sudo pacman -Syu
 
 # Hyprland base
 sudo pacman -S --noconfirm hyprland
