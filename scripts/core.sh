bf938d33ddc5d8a6bbe9741c706f3b5da139a142
Matt <metwas.seup@Gmail.com>
2026-01-12 18:39:21 +0000

    Update core.sh

--- scripts/core.sh
+++ scripts/core.sh
@@ -5,11 +5,11 @@ sudo pacman -Sc --noconfirm
 sudo pacman-key --populate archlinux
 sudo pacman -Sy --noconfirm archlinux-keyring
 sudo pacman -Syu --noconfirm
-sudo pacman -Sy --noconfirm iwd
-sudo pacman -S --noconfirm wine winetricks sdl2-compat gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg samba gnutls lib32-alsa-oss
-sudo pacman -S --noconfirm pipewire pipewire-pulse pipewire-alsa lib32-pipewire lib32-pipewire-pulse lib32-libpulse lib32-alsa-lib lib32-alsa-plugins
-sudo pacman -S --noconfirm vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa
-sudo pacman -S --noconfirm --needed gcc-libs lib32-gcc-libs glibc lib32-glibc
+sudo pacman -Sy --needed --noconfirm iwd
+sudo pacman -S --needed --noconfirm wine winetricks sdl2-compat gst-plugins-base-libs gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg samba gnutls lib32-alsa-oss
+sudo pacman -S --needed --noconfirm pipewire pipewire-pulse pipewire-alsa lib32-pipewire lib32-pipewire-pulse lib32-libpulse lib32-alsa-lib lib32-alsa-plugins
+sudo pacman -S --needed --noconfirm vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa
+sudo pacman -S --needed --noconfirm --needed gcc-libs lib32-gcc-libs glibc lib32-glibc
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed --noconfirm \
   webkit2gtk-4.1 \
@@ -24,12 +24,12 @@ sudo pacman -S --needed --noconfirm \
   xdotool
 
 # Hyprland base
-sudo pacman -S --noconfirm hyprland
-sudo pacman -S --noconfirm rofi-wayland
-sudo pacman -S --noconfirm hyprpaper
-sudo pacman -S --noconfirm hyprlock
-sudo pacman -S --noconfirm xdg-desktop-portal-hyprland
-sudo pacman -S --noconfirm xorg-xwayland
+sudo pacman -S --needed --noconfirm hyprland
+sudo pacman -S --needed --noconfirm rofi-wayland
+sudo pacman -S --needed --noconfirm hyprpaper
+sudo pacman -S --needed --noconfirm hyprlock
+sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
+sudo pacman -S --needed --noconfirm xorg-xwayland
 
 # Tools
-sudo pacman -S --noconfirm nmap
+sudo pacman -S --needed --noconfirm nmap
