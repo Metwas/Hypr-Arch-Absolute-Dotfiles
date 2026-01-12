29b2192f71084b0a85cf48e68d604103963c970f
Matt <metwas.seup@Gmail.com>
2026-01-12 18:39:59 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,19 +22,19 @@ ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin metwas %I $TERM
 EOF
 
 # Drivers
-sudo pacman -S --noconfirm mesa
-sudo pacman -S --noconfirm xf86-video-amdgpu
-sudo pacman -S --noconfirm vulkan-radeon
-sudo pacman -S --noconfirm libva-mesa-driver
+sudo pacman -S --needed --noconfirm mesa
+sudo pacman -S --needed --noconfirm xf86-video-amdgpu
+sudo pacman -S --needed --noconfirm vulkan-radeon
+sudo pacman -S --needed --noconfirm libva-mesa-driver
 
 # Apps
-yay -S --noconfirm thorium-browser-bin
-yay -S --noconfirm zed
-yay -S --noconfirm esp-idf
-yay -S --noconfirm cava
-yay -S --noconfirm yazi
-yay -S --noconfirm hollywood
-yay -S --noconfirm tty-clock
+yay -S --needed --noconfirm thorium-browser-bin
+yay -S --needed --noconfirm zed
+yay -S --needed --noconfirm esp-idf
+yay -S --needed --noconfirm cava
+yay -S --needed --noconfirm yazi
+yay -S --needed --noconfirm hollywood
+yay -S --needed --noconfirm tty-clock
 
 # ZED custom THEME
 sudo mkdir ./.config/zed
