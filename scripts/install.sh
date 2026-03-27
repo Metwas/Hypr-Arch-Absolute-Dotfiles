9d1a91d0f4947d93043209aa7f05be360ea73107
Matt <metwas.seup@Gmail.com>
2026-03-27 16:40:33 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,6 +32,29 @@ sudo pacman -S --needed --noconfirm \
   librsvg \
   xdotool
 
+# Essential networking
+sudo pacman -S --needed --noconfirm iwd
+
+sudo mkdir /etc/iwd
+sudo mkdir /etc/systemd/resolved.conf.d
+
+sudo cat <<EOF | sudo tee /etc/iwd/main.conf
+[General]
+EnableNetworkConfiguration=true
+
+[Network]
+NameResolvingService=systemd
+EOF
+
+sudo cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+[Resolve]
+DNS=8.8.8.8 1.1.1.1 1.0.0.1
+Domains=~.
+EOF
+
+sudo systemctl enable iwd.service
+sudo systemctl enable systemd-resolved.service
+
 # Hyprland base
 sudo pacman -S --needed --noconfirm hyprland
 sudo pacman -S --needed --noconfirm rofi-wayland
