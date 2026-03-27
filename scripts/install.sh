e1c61ecda7fa3868bfd89e0679ae8eeb91e6b347
Matt <metwas.seup@Gmail.com>
2026-03-27 16:38:29 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,20 +26,18 @@ sudo pacman -S --needed --noconfirm mesa
 sudo pacman -S --needed --noconfirm xf86-video-amdgpu
 sudo pacman -S --needed --noconfirm vulkan-radeon
 sudo pacman -S --needed --noconfirm libva-mesa-driver
+sudo pacman -S --needed --noconfirm pipewire pipewire-pulse pipewire-alsa lib32-pipewire lib32-pipewire-pulse lib32-libpulse lib32-alsa-lib lib32-alsa-plugins
 
 # Apps
-yay -S --needed --noconfirm thorium-browser-bin
+yay -S --needed --noconfirm brave-bin
 yay -S --needed --noconfirm zed
-yay -S --needed --noconfirm esp-idf
 yay -S --needed --noconfirm cava
 yay -S --needed --noconfirm yazi
-yay -S --needed --noconfirm hollywood
 yay -S --needed --noconfirm tty-clock
 
 # ZED custom THEME
 sudo mkdir ./.config/zed
 sudo mkdir ./.config/zed/themes
-git clone https://github.com/Metwas/nightfox.zed.git ./.config/zed/themes
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
